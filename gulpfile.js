c6df0a72037b099cfe3b8c82c35766f9a6b9c544
aadlhq <farren.esman@aadl.org.au>
2019-04-23 09:41:38 +1000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+	//watch = require('gulp-watch'),
+	pug = require('gulp-pug')
+
+var config = {
+	'pug': {
+		'srcPath': 'src/**/*.pug',
+		'outputPath': 'output/html'
+	}
+};
+
+gulp.task('pug', function(){
+	return gulp.src( config.pug.srcPath )
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest( config.pug.outputPath ))
+});
+
+gulp.task('watch', function(){
+	gulp.watch( config.pug.srcPath )
+		.on( 'change', function( path, stats ) {
+			console.log( path );
+		} )
+});
+
+
+//gulp.watch( config.pug.srcPath , ['pug']);
+
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+	// gulp.src( config.pug.srcPath )
+	//   .pipe(pug())
+	//   .pipe(gulp.dest(config.pug.outputPath))
+	gulp.watch( config.pug.srcPath, gulp.task('pug') )
+  done();
+}
