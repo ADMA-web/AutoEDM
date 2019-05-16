970c40b2f1e8a1d1b2a73e204be188be5c8301f3
aadlhq <farren.esman@aadl.org.au>
2019-05-16 10:52:14 +1000

    template updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp'),
-	pug = require('gulp-pug')
+var gulp = require( 'gulp' ),
+	pug = require( 'gulp-pug' ),
+	browserSync = require( 'browser-sync' )
+	reload = browserSync.reload
 
 var config = {
 	'pug': {
@@ -20,6 +22,7 @@ gulp.task('watch', function(){
 	gulp.watch( config.pug.srcPath )
 		.on( 'change', function( path, stats ) {
 			console.log( path );
+			pugify();
 		} )
 });
 
@@ -28,20 +31,6 @@ function pugify() {
 		.src( config.pug.srcPath )
 		.pipe( pug() )
 		.pipe( gulp.dest( config.pug.outputPath ) );
-}
-
-
-//gulp.watch( config.pug.srcPath , ['pug']);
-
-
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-	// gulp.src( config.pug.srcPath )
-	//   .pipe(pug())
-	//   .pipe(gulp.dest(config.pug.outputPath))
-	gulp.watch( config.pug.srcPath, gulp.task('pug') )
-  done();
 }
 
 exports.pugify = pugify;
